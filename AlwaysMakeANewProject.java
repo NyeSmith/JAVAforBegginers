c86730f530524ff27f968c69aac3bedca9ffd2b1
NyeSmith <Asmith@utcmediacityuk.org.uk>
2016-09-13 10:41:14 +0100

    Add files via upload

---
+++ AlwaysMakeANewProject.java
@@ -0,0 +1,64 @@
+package always.make.a.pkgnew.project;
+public class AlwaysMakeANewProject {
+
+    public static void main(String[] args) {
+
+        // My strings
+        String colour = "green";
+        String name = "Nye";
+        String street = "52 Style Road";
+        String DOB = "15th Feb 2000";
+
+        // Say my strings
+            System.out.println(colour);
+            System.out.println(name);
+            System.out.println(street);
+            System.out.println(DOB);
+
+        // Number stuffff
+           int number = 1000000000;
+           int age = 16;
+           int ShoeSize = 2;
+
+        // Say my number with statement
+            System.out.println("your chosen number was:"+ number);
+            System.out.println("your age is:"+ age);
+            System.out.println("Your shoe size is:"+ ShoeSize);
+
+        // Numbers with decimals
+           double test = 1.5;
+           double pi = 3.14;
+        // Say number with decimals
+            System.out.println(test);
+            System.out.println(pi);
+        
+        // area of circle = pi*R^2
+        // speed = distance/time
+            
+              //##########################//
+              //#    NOTES:              #//
+              //#    Multiply = *        #//
+              //#    Divide = /          #//
+              //##########################//
+        
+            System.out.println(age + ShoeSize);
+            System.out.println(age - ShoeSize);
+            System.out.println(age * ShoeSize);
+            System.out.println(age / ShoeSize);
+            System.out.println(age *3);
+            
+            int x = 12
+            int y = 11
+            
+            if (x < y) {
+            //if the condition is true
+            System.out.println("X is less than y");
+                
+            }else{
+            //if the condition is false
+                System.out.println("X is more than Y");
+            
+            }
+    }
+    
+}
